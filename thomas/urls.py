2b56fda944e1d4bfccb5c91e19fac48b57f6f25b
josemar7 <josemar7@gmail.com>
2016-05-30 00:16:40 +0200

    initial commit

--- thomas/urls.py
+++ thomas/urls.py
@@ -1,6 +1,24 @@
-from django.conf.urls import url
+# .. Imports
+from django.conf.urls import patterns, url, include
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet, LoginView, LogoutView
 from thomas.views import IndexView
 
-urlpatterns = [
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+
+
+urlpatterns = patterns(
+     '',
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
-]
+
+)
