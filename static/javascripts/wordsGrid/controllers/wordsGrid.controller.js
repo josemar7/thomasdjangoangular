fba558f2a217ae45d22169da05644d0da3e85ff3
josemar7 <josemar7@gmail.com>
2016-06-22 14:11:11 +0200

    commit!

--- static/javascripts/wordsGrid/controllers/wordsGrid.controller.js
+++ static/javascripts/wordsGrid/controllers/wordsGrid.controller.js
@@ -7,7 +7,13 @@
 
   angular
     .module('thomas.wordsGrid.controllers')
-    .controller('WordsGridController', WordsGridController);
+    .controller('WordsGridController', WordsGridController)
+    .filter('toUpperCase', function () {
+      return function (value) {
+        return value.toUpperCase();
+      };
+    });
+
 
 WordsGridController.$inject = ['$scope', '$http', 'ngDialog', 'words', 'Utils'];
 
@@ -18,15 +24,20 @@ WordsGridController.$inject = ['$scope', '$http', 'ngDialog', 'words', 'Utils'];
   function WordsGridController($scope, $http, ngDialog, words, Utils) {
 
       $scope.gridOptions = {
-        paginationPageSizes: [25, 50, 75],
+        //paginationPageSizes: [25, 50, 75],
+        enablePaginationControls: false,
         paginationPageSize: 25,
         columnDefs: [
-          { name: Utils.getMessage('NAME'), field: 'name' },
-          { name: Utils.getMessage('TRANSLATION'), field: 'translation' }
+          { name: Utils.getMessage('NAME'), field: 'name', cellTooltip: true, cellFilter: 'toUpperCase' },
+          { name: Utils.getMessage('TRANSLATION'), field: 'translation', cellTooltip: true, cellFilter: 'toUpperCase' }
         ]
       };
 
       $scope.gridOptions.data = words.data;
+
+      $scope.gridOptions.onRegisterApi = function (gridApi) {
+        $scope.gridApi = gridApi;
+      }
 
     }
 
