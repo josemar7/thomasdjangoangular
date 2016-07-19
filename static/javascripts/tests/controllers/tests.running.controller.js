1378b67152199c83f8eebba1f90b50d15f54c01b
josemar7 <josemar7@gmail.com>
2016-07-19 19:15:28 +0200

    initial commit

--- static/javascripts/tests/controllers/tests.running.controller.js
+++ static/javascripts/tests/controllers/tests.running.controller.js
@@ -21,18 +21,25 @@
             Tests.test(Number(words_number)).then(function (response) {
                 $scope.list_words = response.data;
                 $scope.current_index = 0;
-                $scope.cols =  $scope.list_words[$scope.current_index].name.length * 100 / 5;
-                $log.log($scope.list_words[$scope.current_index].name.length);
-                $log.log($scope.cols);
-                //$scope.cols =  5 * 20;
+                setWordSettings($scope.test.modality, $scope.list_words[$scope.current_index]);
             });
         }
 
         $scope.load($scope.$parent.test.words_number, $scope);
 
-        function getSize() {
-
-
+        function setWordSettings(modality, word) {
+            if (modality === 'en_es') {
+                $scope.name = word.name;
+                $scope.value = word.translation;
+                $scope.cols_name =  word.name.length * 100 / 5;
+                $scope.cols_value =  word.translation.length * 100 / 5;
+            }
+            else if ($scope.test.modality === 'es_en') {
+                $scope.name = word.translation;
+                $scope.value = word.name;
+                $scope.cols_name =  word.translation.length * 100 / 5;
+                $scope.cols_value =  word.length * 100 / 5;
+            }
         }
 
   }
